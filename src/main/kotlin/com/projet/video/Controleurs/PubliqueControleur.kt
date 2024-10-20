8561a3a8530566f143353d43003caa6082523f86
LeslyJunior Gourdet <lgourdet@ordralphabetix>
2024-10-20 11:59:12 -0400

    Structure des contrôleurs et des modèles de données

---
+++ src/main/kotlin/com/projet/video/Controleurs/PubliqueControleur.kt
@@ -0,0 +1,9 @@
+package com.projet.video.Controleurs
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PubliqueController {
+}
