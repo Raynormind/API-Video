9a660c4050c798936d141bd73c5ac846397bb4ea
LeslyJunior Gourdet <lgourdet@ordralphabetix>
2024-10-23 16:52:41 -0400

    Suppression de publiqueControler et mise à jour de admin et utilisateur

--- src/main/kotlin/com/projet/video/Controleurs/AdminControleur.kt
+++ src/main/kotlin/com/projet/video/Controleurs/AdminControleur.kt
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 @RestController
+@RequestMapping("/video")
 class AdminController {
 
     @PutMapping("/videos/{id_video}?statut={statut}")
