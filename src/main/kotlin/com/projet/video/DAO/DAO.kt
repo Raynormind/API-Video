8104fa332ef079ee9f8c123c44dee158db23837e
Jacob Zapitoski <jzapitoski@ordralphabetix>
2024-11-22 10:15:20 -0500

    element renomer en video dans dao

--- src/main/kotlin/com/projet/video/DAO/DAO.kt
+++ src/main/kotlin/com/projet/video/DAO/DAO.kt
@@ -8,7 +8,7 @@ interface DAO<T> {
     fun chercherParTitre(titre: String): List<T>
     fun chercherParAuteur(auteur: Utilisateur): List<T>
     fun chercherParStatut(statut: String): List<T>
-    fun ajouter(element: T): T?
-    fun modifier(id_video: Int, element: T): T?
+    fun ajouter(video: T): T?
+    fun modifier(id_video: Int, video: T): T?
     fun effacer(id_video: Int)
 }
